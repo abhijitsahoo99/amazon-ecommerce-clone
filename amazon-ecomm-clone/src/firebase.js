604e469c31086f733d31cc5b3306f6424f2d0da8
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-09-20 00:47:00 +0530

    Added initial file and made non-functional header/navbar

---
+++ amazon-ecomm-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDO7vtS2lYxUNq_u3a3dpaQEfzJW_1PMIE",
+  authDomain: "ecomm-clone-c215f.firebaseapp.com",
+  projectId: "ecomm-clone-c215f",
+  storageBucket: "ecomm-clone-c215f.appspot.com",
+  messagingSenderId: "814081440840",
+  appId: "1:814081440840:web:e06e900c9bce7e5008ec7d",
+  measurementId: "G-MYDJT9PZEM"
+};
