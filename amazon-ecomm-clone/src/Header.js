a93ac5a3caa462e8b9f26eb00a8349cc1cb7f478
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-09-30 00:35:12 +0530

    Added usecontex and Reducer in index.js for checkout page -> product and header

--- amazon-ecomm-clone/src/Header.js
+++ amazon-ecomm-clone/src/Header.js
@@ -4,8 +4,10 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import {useNavigate} from "react-router-dom";
 import {Link} from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue();
     const navigate = useNavigate()
   return (
     <div className='header'>
@@ -35,7 +37,7 @@ function Header() {
             <ShoppingBasketIcon className= "shopping_basket" onClick = {() => {
                 navigate("/checkout")
             }} />
-            <span className='header_optionLineTwo header_basketCount'>0</span>
+            <span className='header_optionLineTwo header_basketCount'>{basket?.length}</span>
         </div>
     </div>
     </div>
--- amazon-ecomm-clone/src/Product.jsx
+++ amazon-ecomm-clone/src/Product.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
 function Product({id , title, price, rating , image}) {
+  const [{basket}, dispatch] = useStateValue();
+  const addToBasket = () => {
+    // dispatch the item into data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id : id,
+        title : title,
+        image : image,
+        price : price,
+        rating : rating,
+      },
+    });
+  };
   return (
     <div className='product'>
         <div className='product_info'>
@@ -18,7 +33,7 @@ function Product({id , title, price, rating , image}) {
             </div>
         </div>
         <img src={image} alt='product-display'></img>
-        <button>Add To Basket</button>
+        <button onClick={addToBasket}>Add To Basket</button>
       
     </div>
   )
